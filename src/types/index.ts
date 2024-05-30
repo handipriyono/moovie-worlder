06212a538aaf38808216ea50d1c025968bb28031
Handi Priyono <handimacbook@gmail.com>
2024-05-30 22:40:26 +0700

    feat: add types for movie item

---
+++ src/types/index.ts
@@ -0,0 +1,10 @@
+export type TMovieItem = {
+  title: string;
+  id: number;
+  vote_average: number;
+  original_title: string;
+  original_language: string;
+  overview: string;
+  release_date: string;
+  poster_path: string;
+};
