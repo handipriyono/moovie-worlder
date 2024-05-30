c1e53b30dd733b2447fcbdbb00e5ab5bba0b740e
Handi Priyono <handimacbook@gmail.com>
2024-05-30 23:51:47 +0700

    Feat: update genre hooks

--- src/hooks/useSearch.ts
+++ src/hooks/useSearch.ts
@@ -1,10 +1,11 @@
-import { getSearchMovies } from "@/api/index";
+import { getSearchMovies, getGenre } from "@/api/index";
 import { useQuery } from "@tanstack/react-query";
 import useSearchStore from "@/stores/search";
 import { useShallow } from "zustand/react/shallow";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export default function useSearch() {
+  const [genres, setGenres] = useState({});
   const { search, setSearch } = useSearchStore(
     useShallow((state) => ({
       search: state.search,
@@ -16,6 +17,27 @@ export default function useSearch() {
     queryFn: () => getSearchMovies(search),
     enabled: !!search,
   });
+
+  const { data: dataGenre } = useQuery({
+    queryKey: ["list-genre", search],
+    queryFn: getGenre,
+    staleTime: 900000,
+  });
+
+  const arrayToObject = (arr: { id: number; name: string }[]) => {
+    const obj: { [key: number]: { id: number; name: string } } = {};
+    arr.forEach((item) => {
+      obj[item.id] = item;
+    });
+    return obj;
+  };
+
+  useEffect(() => {
+    if (dataGenre) {
+      const item = arrayToObject(dataGenre);
+      setGenres(item);
+    }
+  }, [dataGenre]);
 
   useEffect(() => {
     setSearch("");
@@ -29,5 +51,6 @@ export default function useSearch() {
     moviesSearch: data,
     search,
     onHandleSearch,
+    genres,
   };
 }
