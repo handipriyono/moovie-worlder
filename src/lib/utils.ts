36e8ed14bedcc30240e3b2b2a9be84626625f8ff
Handi Priyono <handimacbook@gmail.com>
2024-05-30 23:50:36 +0700

    feat: add lib for movie and type genre

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,10 +1,15 @@
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 import axios from "axios";
+import { TGenre } from "@/types";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export const getGenre = (genreItem: Array<number>, genreList: TGenre) => {
+  return genreItem?.map((item) => genreList?.[item]?.name).join(", ");
+};
 
 export const axiosAPI = axios.create({
   baseURL: "https://api.themoviedb.org/3",
